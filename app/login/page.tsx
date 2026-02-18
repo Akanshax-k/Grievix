1b3ba30c433a90babbd23e9f1dfeb39944ec9c84
kunal patel <kunno1751@gmail.com>
2026-02-19 00:23:34 +0530

    fix the login and report page

---
+++ app/login/page.tsx
@@ -0,0 +1,12 @@
+import Login from '@/components/ui/Signin'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <Login />
+    </div>
+  )
+}
+
+export default page
---
+++ app/report/page.tsx
@@ -0,0 +1,12 @@
+import Report from '@/components/ui/Report'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <Report />
+    </div>
+  )
+}
+
+export default page
